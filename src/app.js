ebc55acc4369644ed2d70e5bf0b2c66a32cee559
Gskull <Gskull47@gmail.com>
2022-04-11 01:03:02 -0700

    added a textarea, save button, and a delete button into the note div also made a if statement so you cant make more then one note div at the same time

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-let clone = false
+let noteInProgress = false
 const button = document.querySelector('.icons');
 button.addEventListener('click', addNotepad);
-let noteContainer = document.getElementsByClassName("write-note-area")[0]
+const noteContainer = document.getElementsByClassName("write-note-area")[0]
 
 function addNotepad(){
-  const newNote = document.createElement("div")
-  newNote.classList.add('Note')
-  noteContainer.appendChild(newNote)
+  if (noteInProgress === false){      //looks to see if there is a note being worked on.
+    let newNote = document.createElement("div")      //makes the div
+    newNote.classList.add('Notediv')                    //gives the div a class
+    noteContainer.appendChild(newNote)                  //makes the div go where its sapost to
 
+    const noteDiv = document.getElementsByClassName("Notediv")[0]  //finds the note div 
+
+    newNote = document.createElement("textarea")
+    newNote.classList.add('Notepad')
+    noteDiv.appendChild(newNote)
+
+    newNote = document.createElement("button");
+    newNote.classid.add('save')
+    btn.innerHTML = "Save";
+    noteDiv.appendChild(newNote);
+
+    newNote = document.createElement("button");
+    newNote.classid.add('delete')
+    btn.innerHTML = "Delete";
+    noteDiv.appendChild(newNote);
+
+  }
 }
