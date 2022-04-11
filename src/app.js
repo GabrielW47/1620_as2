de03159968ee18c7b51c50aaee7ae55fe67c8468
Gskull <Gskull47@gmail.com>
2022-04-11 13:25:38 -0700

    added a function to the delete button in js so that it deletes the notediv with everythin in it

--- src/app.js
+++ src/app.js
@@ -16,15 +16,34 @@ function addNotepad(){
     textpad.classList.add('Notepad')                      //gives the textarea a class
     noteDiv.appendChild(textpad)                         //makes it go into the note div
 
-    saveButton = document.createElement("button");
-    saveButton.classList.add('save')
-    saveButton.innerHTML = "Save";
-    noteDiv.appendChild(saveButton);
+    saveButton = document.createElement("button");           //makes a button
+    saveButton.classList.add('save')                         //gives the button a class
+    saveButton.innerHTML = "Save";                           //makes the button say Save
+    noteDiv.appendChild(saveButton);                         //puts the button in the note div
 
-    deletebutton = document.createElement("button");
-    deletebutton.classList.add('delete')
-    deletebutton.innerHTML = "Delete";
-    noteDiv.appendChild(deletebutton);
+    deletebutton = document.createElement("button");         //makes a button
+    deletebutton.classList.add('delete')                     //gives the button a class
+    deletebutton.innerHTML = "Delete";                       //makes the button say Delete
+    noteDiv.appendChild(deletebutton);                       //puts the button in the note div
 
   }
 }
+document.addEventListener('click', e =>{
+  if (noteInProgress  === true){
+    const Deletebutton = document.querySelector('.delete');
+    Deletebutton.addEventListener('click', Delete);
+  
+    function Delete(){
+      console.log("woked")
+      const noteDiv = document.getElementsByClassName("Notediv")[0]
+      //noteDiv.removeChild("Notepad");
+      //noteDiv.removeChild("save");
+      //noteDiv.removeChild("delete");
+      noteDiv.remove();
+    }
+  
+  }
+
+}, capture = true);
+
+
