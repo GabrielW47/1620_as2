df87b3b89e55c96781862742695061085b816e36
Gskull <Gskull47@gmail.com>
2022-04-11 01:21:57 -0700

    fixed size of textarea by making it biger with a flexbox added some temperary styling

--- src/app.js
+++ src/app.js
@@ -5,15 +5,16 @@ const noteContainer = document.getElementsByClassName("write-note-area")[0]
 
 function addNotepad(){
   if (noteInProgress === false){      //looks to see if there is a note being worked on.
+    noteInProgress = true
     let newNote = document.createElement("div")      //makes the div
     newNote.classList.add('Notediv')                    //gives the div a class
     noteContainer.appendChild(newNote)                  //makes the div go where its sapost to
 
     const noteDiv = document.getElementsByClassName("Notediv")[0]  //finds the note div 
 
-    newNote = document.createElement("textarea")
-    newNote.classList.add('Notepad')
-    noteDiv.appendChild(newNote)
+    newNote = document.createElement("textarea")     //makes the textarea
+    newNote.classList.add('Notepad')                 //gives the textarea a class
+    noteDiv.appendChild(newNote)                     //makes it go into the note div
 
     newNote = document.createElement("button");
     newNote.classid.add('save')
