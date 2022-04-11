4b743dedf8df036db2ec7a7ee47afae4320ee7ce
Gskull <Gskull47@gmail.com>
2022-04-11 00:29:03 -0700

    div from + button now goes to the write-note-area useing js file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 let clone = false
-
 const button = document.querySelector('.icons');
-
 button.addEventListener('click', addNotepad);
+let noteContainer = document.getElementsByClassName("write-note-area")[0]
 
 function addNotepad(){
   const newNote = document.createElement("div")
-  document.body.appendChild(newNote)
+  newNote.classList.add('Note')
+  noteContainer.appendChild(newNote)
+
 }
